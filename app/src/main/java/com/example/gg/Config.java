cd5c829aab35be07baad7112f0f6d1b5ce9190ff
Fox <mr.blind.fox.tv@gmail.com>
2019-07-11 23:49:18 +0300

    Wi-Fi Checking

---
+++ app/src/main/java/com/example/gg/Config.java
@@ -0,0 +1,7 @@
+package com.example.gg;
+
+public class Config {
+    public static final String FTP_HOST = "";
+    public static final String WIFI_SSID = "4ipNet_Internan";
+    public static Boolean FTP_ENABLED = true;
+}
