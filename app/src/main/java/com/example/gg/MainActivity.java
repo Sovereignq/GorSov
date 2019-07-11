6c5c7e390e910e6b110365f8fa0f4d6dbfdb5815
Sovereign <boyko.rusya.03@gmail.com>
2019-07-11 12:56:00 +0300

    Button Enter changed

--- app/src/main/java/com/example/gg/MainActivity.java
+++ app/src/main/java/com/example/gg/MainActivity.java
@@ -1,14 +1,68 @@
 package com.example.gg;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     MainActivity a;
+    EditText login, password;
+    Button enter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+        login = findViewById(R.id.editTextLogin);
+        password = findViewById(R.id.editTextPassword);
+        enter = findViewById(R.id.btnEnter);
+        enter.setEnabled(false);
+        login.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!login.getText().toString().isEmpty() && !password.getText().toString().isEmpty()) {
+                    enter.setTextColor(Color.rgb(255,255,255));
+                    enter.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(!login.getText().toString().isEmpty() && !password.getText().toString().isEmpty()) {
+                    enter.setTextColor(Color.rgb(255,255,255));
+                    enter.setEnabled(true);
+                }
+            }
+        });
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!login.getText().toString().isEmpty() && !password.getText().toString().isEmpty()) {
+                    enter.setTextColor(Color.rgb(255,255,255));
+                    enter.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(!login.getText().toString().isEmpty() && !password.getText().toString().isEmpty()) {
+                    enter.setTextColor(Color.rgb(255,255,255));
+                    enter.setEnabled(true);
+                }
+            }
+        });
     }
 }
